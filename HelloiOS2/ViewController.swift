36bd74da6bf3edb102385182ee03ec41b6818092
talos <talos@unam.mx>
2024-11-30 10:20:58 -0600

    Initial Commit

---
+++ HelloiOS2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloiOS2
+//
+//  Created by Edgar Vargas on 30/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
